a68f8718073052b889da7b5bbadfe8ab6f066383
tanchaoju <1120153473@qq.com>
2020-01-02 19:48:26 +0800

    机票列表页面动态渲染机票列表

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -10,7 +10,7 @@
       <!-- 搜索表单 -->
       <div>
         <SearchForm></SearchForm>
-        </div>
+      </div>
 
       <!-- banner广告 -->
       <div class="sale-banner">
@@ -40,15 +40,48 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale"></div>
+    <el-row type="flex" class="air-sale-pic" justify="space-between">
+      <el-col :span="6" v-for="(item, index) in sales" :key="index">
+        <nuxt-link
+          :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+        >
+          <img :src="item.cover" />
+          <el-row class="layer-bar" type="flex" justify="space-between">
+            <span>{{item.departCity}}-{{item.destCity}}</span>
+            <span>￥{{item.price | toFixed}}</span>
+          </el-row>
+        </nuxt-link>
+      </el-col>
+    </el-row>
   </section>
 </template>
 
 <script>
 import SearchForm from "@/components/air/searchForm";
 export default {
+  data() {
+    return {
+      sales: []
+    };
+  },
   components: {
     SearchForm
+  },
+  mounted() {
+    // 发送特价机票请求
+    this.$axios({
+      url: "/airs/sale"
+    }).then(res => {
+      this.sales = res.data.data;
+      console.log(this.sales);
+      
+    });
+  },
+  filters: {
+    // 保留两位小数
+    toFixed(value) {
+      return Number(value).toFixed(2);
+    }
   }
 };
 </script>
