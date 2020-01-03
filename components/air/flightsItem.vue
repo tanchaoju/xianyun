95599d92c08a19050e6453da1b6ea6f6f440671f
tanchaoju <1120153473@qq.com>
2020-01-03 20:57:55 +0800

    订单详情页面跳转与页面结构

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -47,7 +47,11 @@
             </el-col>
             <el-col :span="5" class="price">￥{{item.settle_price }}</el-col>
             <el-col :span="3" class="choose-button">
-              <el-button type="warning" size="mini">选定</el-button>
+              <el-button
+                type="warning"
+                size="mini"
+                @click="handleLinkOrder(data.id, item.seat_xid)"
+              >选定</el-button>
               <p>剩余：{{item.discount}}</p>
             </el-col>
           </el-row>
@@ -91,7 +95,18 @@ export default {
       }
       const hh = Math.floor(dis / 60);
       const mm = dis % 60;
-      return `${hh}时${mm}分`
+      return `${hh}时${mm}分`;
+    }
+  },
+  methods: {
+    handleLinkOrder(id, seat_xid) {
+      this.$router.push({
+         path: "/air/order",
+        query: {
+          id,
+          seat_xid
+        }
+      });
     }
   }
 };
